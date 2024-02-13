96a0baae157fbc4b8796d7343f73c244994f4fb4
PeterSignor <petereysig@gmail.com>
2024-02-12 16:08:21 -0800

    Update multiples.py

---
+++ multiples.py
@@ -0,0 +1,12 @@
+# Print out the multiple of 3 up to 100 
+# count = 0
+# for count in range (1, 100):
+#     if count % 3 == 0:
+#         print (count)
+#     elif count % 5 == 0:
+#         print(count)    
+   
+
+
+# Using the same loop, let's see if we can print out the multiples of 5 too 
+
