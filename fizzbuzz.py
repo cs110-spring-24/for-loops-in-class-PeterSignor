6d5a790b246ee772d683a1af10bc74842ff904b9
PeterSignor <petereysig@gmail.com>
2024-02-12 16:08:19 -0800

    Update fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+count = 0
+for count in range (1, 31):   
+    if count % 15 == 0:  
+        print("FizzBuzz")  
+    elif count % 3 == 0:
+        print ("Fizz")
+    elif count % 5 == 0:
+        print("Buzz")   
+    else:
+        print(count)    
+   
